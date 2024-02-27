d3d9aea9d6b59ba35cdaebbf03f582f66c2e6082
lusamreth <somrethoverkill@gmail.com>
2024-02-27 14:54:19 +0700

    feat: rework hlslens config

--- lua/core/modules/nv-hlsearch.lua
+++ lua/core/modules/nv-hlsearch.lua
@@ -1,30 +1,41 @@
 require('utility.binding')
--- args :-n = go forward the highlight letter
---       -N = go backward the highlight letter
-require('hlslens').setup()
+require('hlslens').setup({
+    override_lens = function(render, posList, nearest, idx, relIdx)
+        local sfw = vim.v.searchforward == 1
+        local indicator, text, chunks
+        local absRelIdx = math.abs(relIdx)
+        if absRelIdx > 1 then
+            indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and '▲' or '▼')
+        elseif absRelIdx == 1 then
+            indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
+        else
+            indicator = ''
+        end
 
-local function toggle_search(mode)
-    local hls = require('hlslens')
-    local command = string.format("execute('normal! ' . v:count1 . '%s')", mode)
-    vim.cmd(command)
-    hls.exportLastSearchToQuickfix()
-end
+        local lnum, col = unpack(posList[idx])
+        if nearest then
+            local cnt = #posList
+            if indicator ~= '' then
+                text = ('[%s %d/%d]'):format(indicator, idx, cnt)
+            else
+                text = ('[%d/%d]'):format(idx, cnt)
+            end
+            chunks = { { ' ', 'Ignore' }, { text, 'HlSearchLensNear' } }
+        else
+            text = ('[%s %d]'):format(indicator, idx)
+            chunks = { { ' ', 'Ignore' }, { text, 'HlSearchLens' } }
+        end
+        render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+    end,
+})
 
-local goforward = function()
-    toggle_search('n')
-end
+local kopts = { noremap = true, silent = true }
 
-local gobackward = function()
-    toggle_search('N')
-end
+vim.api.nvim_set_keymap('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 
-CallerF = Gbinder.bind(goforward)
-CallerB = Gbinder.bind(gobackward)
-
-Nnoremap('m', 'lua CallerF()')
-
-Nnoremap('n', 'lua CallerB()')
-
-Nnoremap('* *', "lua require('hlslens').start()", { silent = true })
-Nnoremap('# #', "lua require('hlslens').start()", { silent = true })
-Nnoremap(',l', "lua vim.cmd('nohl')", { silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
